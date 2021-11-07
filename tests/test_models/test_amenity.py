517bae82a6d52c41c8c865a4d67e15ec247a54a4
afecego <afecego@gmail.com>
2021-11-07 16:48:53 -0500

    Initial tests files

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -4,7 +4,6 @@ Unittest for amenity([..])
 """
 import unittest
 from models.amenity import Amenity
-amenity = __import__('amenity').__init__
 
 
 class TestAmenity(unittest.TestCase):
--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -4,10 +4,9 @@ Unittest for file_storage([..])
 """
 import unittest
 from models.engine.file_storage import FileStorage
-file_storage = __import__('file_storage').__init__
 
 
-class Test__init__(unittest.TestCase):
+class TestFileStorage(unittest.TestCase):
     """
     Test for FileStorage class
     """
